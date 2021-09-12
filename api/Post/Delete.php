a017fde68bb1a134127fe141e223d5a6e7518d45
omoluabidotcom <yyahaya222@yahoo.com>
2021-09-12 03:08:20 +0100

    Access method of updated and delete php filw was changed from post to update and delete respectively

--- api/Post/Delete.php
+++ api/Post/Delete.php
@@ -2,7 +2,7 @@
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-with');
 
 include_once '../../config/Datab.php';
